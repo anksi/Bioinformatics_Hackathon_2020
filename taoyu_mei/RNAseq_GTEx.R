4a683a8ff664b36ee20942814051a75f165002fd
Taoyu Mei <vrw936@arke.binf.ku.dk>
2020-04-25 12:37:11 +0200

    edit

--- taoyu_mei/RNAseq_GTEx.R
+++ taoyu_mei/RNAseq_GTEx.R
@@ -1,11 +1,12 @@
+{
 library(R.utils)
 library(tidyverse)
 library(DESeq2)
 library(readxl)
-# library(CePa)
-library(cmapR)
+# library(cmapR)
+}
 
-setwd("D:/GitHub/Bioinformatics_Hackathon_2020/taoyu_mei")
+# setwd("D:/GitHub/Bioinformatics_Hackathon_2020/taoyu_mei")
 
 
 # download preprocessed RNA-Seq data from GTEx ----------------------------
@@ -13,10 +14,13 @@ setwd("D:/GitHub/Bioinformatics_Hackathon_2020/taoyu_mei")
 download.file("https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz", 
               destfile = "./GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz")
 
-R.utils::gunzip("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz", remove = FALSE)
-# geneCounts <- read_tsv("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
+R.utils::gunzip("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz")
+geneCounts <- read_tsv("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct",
+                       skip = 2)
 # geneCounts <- read.gct("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
-geneCounts <- parse_gctx("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
+# geneCounts <- cmapR::parse_gctx("GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
+
+
 
 # subset the count matrix by patients included ---------------------------
 
